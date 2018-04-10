34639bc48fec8cbeb3c80961e0aadfa7bfc62f98
yajunchu <337433736@qq.com>
2018-04-11 00:10:38 +0800

    鉁� introducing new features:

---
+++ src/main/java/com/strawman/blog/config/MybatisPlusConfig.java
@@ -0,0 +1,56 @@
+package com.strawman.blog.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.mybatisplus.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created by yj on 2018/4/10.
+ */
+@Configuration
+@EnableTransactionManagement(order = 2)//由于引入多数据源，所以让spring事务的aop要在多数据源切换aop的后面
+@MapperScan(basePackages = {"mapping"})
+public class MybatisPlusConfig {
+    @Autowired
+    DruidProperties druidProperties;
+
+    /**
+     * 数据源
+     */
+    private DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        druidProperties.config(dataSource);
+        return dataSource;
+    }
+
+    /**
+     * 单数据源连接池配置
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "guns", name = "muti-datasource-open", havingValue = "false")
+    public DruidDataSource singleDatasource() {
+        return dataSource();
+    }
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁mybatis插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
+}
